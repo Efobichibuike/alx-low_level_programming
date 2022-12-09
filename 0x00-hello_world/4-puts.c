fb614e4cbeddee9994600726d107b6300dd111aa
Efobichibuike <omnione079@gmail.com>
2022-12-08 18:31:25 -0800

    task four

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main: Main function of my project to prints on the standard output prgramming is like building a multilingual puzzle
+ *
+ * Return : return 0 everything
+ */
+int main(void)
+{
+	char displayed[52];
+	strcpy(displayed, "\"programming is like building a multiple puzzle");
+	puts(displayed);
+	return (0);
+}
