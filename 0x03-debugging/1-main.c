6e45968fa9b995c9c75b82530232ab12b4cc9047
Efobichibuike <omnione079@gmail.com>
2022-12-16 02:12:26 -0800

    taske 1

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - cuses an infinte loop
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	printf("infinite loop incoming:(\n");
+
+	i = 0;
+	/*
+	 * while (i < 10)
+	 * {
+	 *}
+	 */
+	printf("Infinite loop avoided! \\o/\n");
+
+	return (0);
+}
